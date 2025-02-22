feef15d4e44ed1a2ac7942a4e81ff5f6fcfb3f57
RolandoVeraE <rveraespinoza@gmail.com>
2025-02-22 19:27:13 -0300

    Repositorio iniciado primer commint

---
+++ app.js
@@ -0,0 +1,9 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function ingresoAmigos () {
+    let document.queryByID()
+    
+    if (amigos[indice])
+        alert ("Debe ingresar un nombre")
+}
